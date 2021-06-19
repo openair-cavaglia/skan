9a7c0f1ac5d23c24f1835fe2a22bdd5e13e1ec9b
Giorgio Giuffrè <giorgiogiuffre23@gmail.com>
2021-06-19 10:51:12 +0200

    Format source code

--- src/qrCodeScanner.js
+++ src/qrCodeScanner.js
@@ -7,32 +7,30 @@ const outputData = document.getElementById("outputData");
 const btnScanQR = document.getElementById("btn-scan-qr");
 
 let scanning = false;
-let detection = false;
-let qr_url  = "";
 
 qr.callback = (res) => {
-    if (res) {
-      outputData.innerText = res;
-      scanning = false;
-  
-      video.srcObject.getTracks().forEach(track => {
-        track.stop();
-      });
-  
-      qr.hidden = false;
-      btnScanQR.hidden = false;
-      canvasElement.hidden = true;
-    }
-  };
+  if (res) {
+    outputData.innerText = res;
+    scanning = false;
 
-  const onError = (reason) => {
-    alert("Couldn't find a webcam connected.");
-  };
+    video.srcObject.getTracks().forEach(track => {
+      track.stop();
+    });
 
-  btnScanQR.onclick = () => {
+    qr.hidden = false;
+    btnScanQR.hidden = false;
+    canvasElement.hidden = true;
+  }
+};
+
+const onError = (_) => {
+  alert("Couldn't find a webcam connected.");
+};
+
+btnScanQR.onclick = () => {
   navigator.mediaDevices
     .getUserMedia({ video: { facingMode: "environment" } })
-    .then(function(stream) {
+    .then(function (stream) {
       scanning = true;
       outputData.hidden = false;
       btnScanQR.hidden = true;
@@ -46,34 +44,33 @@ qr.callback = (res) => {
 };
 
 function tick() {
-    canvasElement.height = video.videoHeight;
-    canvasElement.width = video.videoWidth;
-    canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
-  
-    scanning && requestAnimationFrame(tick);
-  }
+  canvasElement.height = video.videoHeight;
+  canvasElement.width = video.videoWidth;
+  canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
 
-  function scan() {
-    try {
-      qr.decode();
-      detection = true; //flag. not used
-      regenerate();
-      scanning = false; // flag. used
-    } catch (e) {
-      setTimeout(scan, 300);
-    }
+  scanning && requestAnimationFrame(tick);
+}
+
+function scan() {
+  try {
+    qr.decode();
+    regenerate();
+    scanning = false;
+  } catch (e) {
+    setTimeout(scan, 300);
   }
+}
 
 function regenerate() {
   try {
-        qr_url = outputData.innerHTML.replace(/&amp;/g,"%26");
-        qr_url = "https://api.qrserver.com/v1/create-qr-code/?data=".concat(qr_url, "&ecc=L&format=svg&size=100x100")
-        var oHideFrame = document.getElementById("2print")
-        oHideFrame.src = qr_url
-        outputData.innerHTML = qr_url
-        console.log(qr_url)
-        window.addEventListener("load", print());
-    } catch (e) {
-      console.log("error in regenerate()")
-    }
-};
+    let qr_url = outputData.innerHTML.replace(/&amp;/g, "%26");
+    qr_url = `https://api.qrserver.com/v1/create-qr-code/?data=${qr_url}&ecc=L&format=svg&size=100x100`;
+    let oHideFrame = document.getElementById("2print");
+    oHideFrame.src = qr_url;
+    outputData.innerHTML = qr_url;
+    console.log(`QR code URL: ${qr_url}`);
+    window.addEventListener("load", print());
+  } catch (e) {
+    console.log(`error in regenerate: ${e}`);
+  }
+}
